65ef8b78a06c90354e00f4675b16dc1e70e2e63c
Saipavan Raju <pavansai.n@gmail.com>
2018-11-26 22:13:41 -0500

    final

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ngMaterial', 'ngMessages','ui.router','ngFileUpload']);
+var app = angular.module('app', ['ngMaterial', 'ngMessages','ui.router','ngFileUpload','ngMaterialDatePicker']);
 app.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/login');
     $stateProvider
@@ -13,125 +13,354 @@ app.config(function($stateProvider, $urlRouterProvider) {
         .state('home', {
           url: '/home',
           templateUrl: '../templates/home.html'
+        })
+        .state('createProgram', {
+          url: '/createProgram',
+          templateUrl: '../templates/createProgram.html'
+        })
+        .state('dataTable', {
+          url: '/dataTable',
+          templateUrl: '../templates/table.html'
         });
       });
 
 app.controller('ctrl',function($scope,$state,$http,$mdToast,$mdDialog){
-  $scope.showLogout = false;
-  $scope.sample = "hello world";
-  $scope.registerUser = {}
-  $scope.loginUser = {}
-  $scope.selectedFolder = null;
-  $scope.items = [1,2,3,4];
-  $scope.newFolderName = null;
-  $scope.userId = null;
-  $scope.userFolders = [];
-  $scope.filesArray = [];
-
+  $scope.registerUser = {};
+  $scope.program = {};
+  $scope.mailingData = {};
+  $scope.subP = false;
   angular.element(document).ready(function () {
+    $scope.scope = JSON.parse(localStorage.getItem('userDetails')).rights;
     let id = localStorage.getItem('userId');
     $scope.userFolders = [];
     if (id.length > 3) {
-      $state.go('home');
       $scope.showLogout = true;
-      $scope.userId = localStorage.getItem('userId');
-      $scope.getFolderNames();
-    }else{
-      $state.go('login');
-      $scope.showLogout = false;
-    }
-  });
-
-  $scope.loginCheck = function(){
-    let id = localStorage.getItem('userId');
-    console.log(id);
-    if (id == null || id == undefined ) {
-      $state.go('login');
-    }
-    if (id.length > 3) {
       $state.go('home');
       $scope.userId = localStorage.getItem('userId');
-      $scope.getFolderNames();
     }else{
+      $scope.showLogout = false;
       $state.go('login');
     }
-  }
-
-  $scope.forgotPassword = function(data){
-    console.log(data);
-    $http.get('http://localhost:3001/forgotPassword/'+$scope.userId+'/'+data).then(function(data){
-      console.log(data);
-      if (data.data.success) {
-        $mdToast.show(
-          $mdToast.simple()
-            .textContent('Decryption Key Mailed to user !!')
-            .position('top right')
-            .hideDelay(3000)
-        );
-      }
-    });
-  }
-
-  $scope.showPrompt = function(ev) {
-  var confirm = $mdDialog.prompt()
-    .title('Enter Key To Encrypt')
-    .textContent('Your file will be encrypted using this key')
-    .initialValue('')
-    .targetEvent(ev)
-    .required(true)
-    .ok('Upload')
-    .cancel('Cancel');
-
-  $mdDialog.show(confirm).then(function(result) {
-    $scope.key = result;
-    var appElement = document.getElementById('uploadCtrl');
-    var appScope = angular.element(appElement).scope();
-    appScope.up.submit();
-  }, function() {
-    $scope.key = '';
   });
-};
-$scope.decryptPrompt = function(ev,file) {
-    $scope.key =''
-    var confirm = $mdDialog.prompt()
-      .title('Enter Key To Decrypt')
-      .textContent('Your file will be decrypted using this key')
-      .initialValue('')
-      .targetEvent(ev)
-      .required(true)
-      .ok('Download')
-      .cancel('Cancel');
 
-    $mdDialog.show(confirm).then(function(result) {
-      $scope.downloadFile(file,result)
-      appScope.up.submit();
-    }, function() {
-      $scope.key = '';
-    });
-};
-
-  $scope.login = function(data){
-    console.log("login");
-    $http.post('http://localhost:3001/signinUser', data).then(function(data){
-      if (data.data.success) {
-        localStorage.setItem('userDetails', JSON.stringify(data.data));
-        localStorage.setItem('userId', data.data.userId);
-          $scope.userId = data.data.userId;
-          $state.go('home');
-      }else{
-        $mdToast.show(
-          $mdToast.simple()
-            .textContent('Invalid Credentials')
-            .position('top right')
-            .hideDelay(3000)
-        );
-      }
+  $scope.example = '           <thead> <tr> <th>Height</th> <th>Weight</th> <th>Age</th> <th>Comment</th> </tr> </thead> <tfoot> <tr> <th>Height</th> <th>Weight</th> <th>Age</th> <th>Comment</th> </tr> </tfoot> '
+  $scope.example2 = '  <thead> <tr> <th>Name</th> <th>Phone</th> <th>User Id</th> <th>Email</th> <th>info </th> </tr> </thead> <tfoot> <tr> <th>Name</th> <th>Phone</th> <th>User Id</th> <th>Email</th> <th> info </th> </tr> </tfoot>'
+  $scope.sendEmailToInstructor = function(){
+    var body = {
+      customerEmail : JSON.parse(localStorage.getItem('userDetails')).email,
+      instructorId : $scope.dataTableInfo.userId,
+      data : $scope.mailingData
+    }
+    $http.post('http://localhost:3004/sendEmail',body).then(function(data){
+        console.log(data);
     });
   }
 
+  $scope.home = function(){
+    $scope.subP = false;
+    $state.go('home');
+    $scope.getPrograms();
+  }
 
+  $scope.logout = function(){
+    localStorage.removeItem('userDetails');
+    localStorage.removeItem('userId');
+    $state.go('login');
+  }
+
+  // Get the modal
+  var modal = document.getElementById('myModal');
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks the button, open the modal
+  btn.onclick = function() {
+      modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  // window.onclick = function(event) {
+  //     if (event.target == modal) {
+  //         modal.style.display = "none";
+  //     }
+  // }
+
+
+
+
+  // $scope.programs =[]
+  function DialogController($scope, $mdDialog) {
+    $scope.modalData = {};
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
+  }
+
+
+  $scope.intModal = function(data){
+    $scope.tempId = data._id;
+
+    $scope.showAdvanced();
+  }
+
+  $scope.sendEmail = function(){
+    modal.style.display = "block";
+
+  }
+
+    $scope.showAdvanced = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: '../templates/dialogue.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+    })
+    .then(function(answer) {
+      $scope.updateCustomerProgramDetails(answer);
+    }, function() {
+
+    });
+  };
+
+
+  $scope.updateCustomerProgramDetails = function(details){
+    var body ={
+      userId : JSON.parse(localStorage.getItem('userDetails')).userId,
+      programId : $scope.tempId,
+      data : details
+     }
+    $http.post('http://localhost:3004/updateCustomerProgramDetails',body).then(function(data){
+
+
+    });
+  }
+  $scope.subP = false;
+
+  $scope.subscribedPrograms = function(){
+    var body ={
+      userId : JSON.parse(localStorage.getItem('userDetails')).userId
+     }
+    $http.post('http://localhost:3004/subscribedPrograms',body).then(function(data){
+      var a =[]
+      for(var i=0 ; i<data.data.length ; i ++){
+        a.push(data.data[i].pData)
+      }
+      $scope.programs = a;
+      for (var i = 0; i < $scope.programs.length; i++) {
+      $scope.programs[i].slots = JSON.parse($scope.programs[i].slots)
+      $scope.programs[i].selectedSlot = data.data[i].selectedSlot
+      }
+      console.log(data);
+      $scope.subP = true;
+
+    });
+  }
+
+  $scope.deleteProgram = function(data,slot){
+    var body ={
+      userId : JSON.parse(localStorage.getItem('userDetails')).userId,
+      data: data,
+      slotNumber : slot
+     }
+     console.log(body);
+    $http.post('http://localhost:3004/deleteProgram',body).then(function(data){
+      $scope.yourPrograms();
+      location.reload();
+    });  };
+
+
+  $scope.infoPage = function(data){
+    $scope.dataTableInfo = data;
+    $state.go('dataTable')
+    console.log(data);
+  }
+
+  $scope.programInfoPage = function(data){
+    $scope.programInfoPageData = data
+    $state.go('dataTable')
+    console.log(data);
+  }
+
+
+
+  $scope.initProgramDataTable = function(){
+    var body = {
+      programId : $scope.programInfoPageData._id
+    }
+
+    $http.post('http://localhost:3004/initProgramDataTable',body).then(function(data){
+      console.log(data.data.data);
+    var dataSet = data.data.data
+    $scope.programsTable = true;
+    // document.getElementById('example2').innerHtml = $scope.example2;
+    $('#example2').DataTable( {
+           data: dataSet,
+           columns: [
+               { name:"Name", data: "name" },
+               { name:"Phone",data: "phone" },
+               { name:"User Id",data: "userId" },
+               { name:"Email",data: "email" },
+               {
+                 "data": null,
+                 "defaultContent": "<button>View</button>"
+               }
+           ]
+       } );
+
+       $('#example2 tbody').on('click', 'td', function () {
+           var table = $('#example2').DataTable();
+           console.log(table.cell( this ).data());
+           // $scope.dataTableInfo = table.cell( this ).data();
+           $scope.initDataTableTwo(table.cell( this ).data());
+       });
+     })
+  }
+
+
+  $scope.initDataTableTwo = function(data){
+    var body = {
+      userId : data.userId,
+      programId : $scope.programInfoPageData._id
+    }
+
+    $http.post('http://localhost:3004/getUserProgramsData',body).then(function(data){
+      console.log(data);
+    var dataSet = data.data.data;
+    $scope.programsTable = false;
+    // document.getElementById('example').innerHtml = $scope.example;
+
+    $('#example').DataTable( {
+           data: dataSet,
+           columns: [
+             { name: "Height", data: "height" },
+             { name: "Weight",data: "weight" },
+             { name: "Age",data: "age" },
+             { name: "Comment",data: "comment" }
+           ],
+           bDestroy: true
+
+       } );
+       // $("#example").dataTable().fnDestroy();
+
+     })
+  }
+
+  $scope.BackToMainTable = function(){
+    $scope.programsTable = true;
+  }
+
+  $scope.initDataTable = function(){
+    var body = {
+      userId : $scope.userId,
+      programId : $scope.dataTableInfo._id
+    }
+
+    $http.post('http://localhost:3004/getUserProgramsData',body).then(function(data){
+      console.log(data);
+    var dataSet = data.data.data
+    // document.getElementById('example').innerHtml = $scope.example;
+    $scope.programsTable = false;
+
+    $('#example').DataTable( {
+           data: dataSet,
+           columns: [
+             { name: "Height", data: "height" },
+             { name: "Weight",data: "weight" },
+             { name: "Age",data: "age" },
+             { name: "Comment",data: "comment" }
+           ],
+            bDestroy: true
+       } );
+
+       // $("#example").dataTable().fnDestroy();
+
+     })
+  }
+
+  $scope.selectProgram = function(data,slot){
+    console.log(data,slot);
+    var body ={
+      userId : JSON.parse(localStorage.getItem('userDetails')).userId,
+      programId : data._id,
+      slotNumber : slot
+    }
+    $http.post('http://localhost:3004/CustomerSelectedProgram',body).then(function(data){
+      if (data.data.success){
+      $mdToast.show(
+        $mdToast.simple()
+          .textContent('Successfully Registered  !!')
+          .position('top right')
+          .hideDelay(3000)
+      );
+    }else if(data.data.success = "registered") {
+      alert("You have already registered !!");
+    }else {
+      alert("The Selected Program is Full.");
+
+    }
+      $state.go('home');
+    });
+  }
+
+  $scope.yourPrograms = function(){
+    var body = {
+      userId : JSON.parse(localStorage.getItem('userDetails')).userId
+    }
+    $http.post('http://localhost:3004/yourPrograms',body).then(function(data){
+      $scope.programs = data.data;
+
+
+      for (var i = 0; i < $scope.programs.length; i++) {
+        $scope.programs[i].slots = JSON.parse($scope.programs[i].slots)
+       }
+      });
+  }
+
+  $scope.getPrograms = function(){
+    $http.get('http://localhost:3004/getPrograms').then(function(data){
+
+      $scope.programs = data.data;
+      console.log($scope.programs);
+
+      for (var i = 0; i < $scope.programs.length; i++) {
+        $scope.programs[i].slots = JSON.parse($scope.programs[i].slots)
+      }
+
+    });
+  }
+
+  $scope.createNewProgram =function(data){
+    data.userId = JSON.parse(localStorage.getItem('userDetails')).userId;
+    $http.post('http://localhost:3004/createNewProgram', data).then(function(data){
+      $state.go('home');
+    });
+  }
+
+  $scope.createProgram = function(){
+    $state.go('createProgram');
+  }
   $scope.signup = function(data){
-    $http.post('http://localhost:3001/registerUser', data).then(function(data){
+    console.log(data);
+    $http.post('http://localhost:3004/registerUser', data).then(function(data){
       if (data.data.success) {
         $mdToast.show(
           $mdToast.simple()
@@ -139,6 +368,8 @@ $scope.decryptPrompt = function(ev,file) {
             .position('top right')
             .hideDelay(3000)
         );
+        $scope.registerUser = {};
+
         $state.go('login');
 
       }else{
@@ -156,58 +387,23 @@ $scope.decryptPrompt = function(ev,file) {
     $state.go('signup');
   }
 
-  $scope.deleteFolder= function(data){
-    console.log(data);
-    $http.get('http://localhost:3001/deleteFolder/'+$scope.userId+'/'+data).then(function(data){
+  $scope.login = function(data){
+    console.log("login");
+    $http.post('http://localhost:3004/signinUser', data).then(function(data){
       if (data.data.success) {
+        localStorage.setItem('userDetails', JSON.stringify(data.data));
+        localStorage.setItem('userId', data.data.userId);
+          $scope.userId = data.data.userId;
+          $state.go('home');
+          location.reload();
+
+      }else{
         $mdToast.show(
           $mdToast.simple()
-            .textContent('Folder Deleted')
+            .textContent('Invalid Credentials')
             .position('top right')
             .hideDelay(3000)
         );
-        $scope.getFolderNames();
-        $scope.newFolderName = null;
-      }
-    });
-  }
-
-  $scope.createFolder= function(data){
-    console.log(data);
-    $http.get('http://localhost:3001/createFolder/'+$scope.userId+'/'+data).then(function(data){
-      if (data.data.success) {
-        $mdToast.show(
-          $mdToast.simple()
-            .textContent('Folder Created')
-            .position('top right')
-            .hideDelay(3000)
-        );
-        $scope.getFolderNames();
-        $scope.newFolderName = null;
-      }
-    });
-  }
-
-  $scope.getFolderNames= function(){
-    $http.get('http://localhost:3001/getFolderNames/'+$scope.userId).then(function(data){
-      $scope.userFolders = data.data.folders;
-      $scope.selectedFolder = data.data.folders[0];
-      $scope.loadFiles();
-
-    });
-  }
-
-  $scope.loadFiles= function(){
-    $http.get('http://localhost:3001/loadFiles/'+$scope.userId+'/'+$scope.selectedFolder).then(function(data){
-      console.log(data);
-      if (data.data.success) {
-        if(!$scope.$$phase) {
-            $scope.$apply(function () {
-              $scope.filesArray = data.data.files;
-            });
-          }else{
-            $scope.filesArray = data.data.files;
-          }
       }
     });
   }
@@ -218,70 +414,4 @@ $scope.decryptPrompt = function(ev,file) {
     $state.go('login');
   }
 
-  $scope.downloadFile = function(data,key){
-    console.log(data);
-    let filedata = data
-    $http.get("http://localhost:3001/checkHash/"+$scope.userId+'/'+$scope.selectedFolder+'/'+data+'/'+key).then(function(data){
-      console.log(data);
-      if (data.data.valid) {
-        window.location.assign(`http://localhost:3001/downloadFile/`+$scope.userId+'/'+$scope.selectedFolder+'/'+filedata+'/'+key);
-      }else {
-        alert('Invalid Decryption Key, Try Again');
-      }
-    });
-  }
-
-  $scope.deleteFile = function(data){
-    console.log(data);
-    $http.get(`http://localhost:3001/deleteFile/`+$scope.userId+'/'+$scope.selectedFolder+'/'+data).then(function(data){
-      console.log(data);
-      if (data.data.success) {
-        $scope.loadFiles();
-
-        $mdToast.show(
-          $mdToast.simple()
-            .textContent('File deleted !!')
-            .position('top right')
-            .hideDelay(3000)
-        );
-      }else{
-        $mdToast.show(
-          $mdToast.simple()
-            .textContent('File could not be deleted !!')
-            .position('top right')
-            .hideDelay(3000)
-        );
-      }
-    });
-  }
-
-  $scope.changeFolder = function(data){
-    console.log(data);
-      $scope.selectedFolder = data
-      $scope.loadFiles();
-  }
 });
-
-app.controller('MyCtrl',['Upload','$window',function(Upload,$window){
-        var vm = this;
-        vm.submit = function(){ //function to call on form submit
-          console.log(".ksdbksdhb");
-            if (vm.upload_form.file.$valid && vm.file) { //check if from is valid
-                vm.upload(vm.file); //call upload function
-            }
-        }
-        vm.upload = function (file) {
-          var appElement = document.querySelector('[ng-controller=ctrl]');
-          var appScope = angular.element(appElement).scope();
-          console.log(appScope.key);
-            Upload.upload({
-                url: 'http://localhost:3001/uploadFile/'+appScope.userId+'/'+appScope.selectedFolder+'/'+appScope.key, //webAPI exposed to upload the file
-                data:{file:file} //pass file as data, should be user ng-model
-            }).then(function (resp) { //upload function returns a promise
-              console.log(resp);
-              if (resp.data.success) {
-                appScope.loadFiles();
-              }
-            });
-        };
-    }]);
